b15bb7550d0086aec0f9088ce35f63be4c477b3e
bwlewis <blewis@illposed.net>
2016-03-31 18:05:44 -0400

    new syntax

--- reference.sh
+++ reference.sh
@@ -4,9 +4,9 @@
 #
 # Test 1: How fast can the generator produce records, without any storage artifacts?
 # 960,000,000 bytes
-time ./generator 10000000 > /dev/null
+time ./generator 10000000 0 > /dev/null
 
 #
 # Test 2: How fast can the generator write to a single storage device on this system?
-time ./generator 10000000 > /home/scidb/scidb_data/0/0/test.bin
+time ./generator 10000000 0 > /home/scidb/scidb_data/0/0/test.bin
 rm -f /home/scidb/scidb_data/0/0/test.bin
